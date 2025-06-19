ed2093823ff66ed15be2043a6bc9f8fd8919a982
Stephen Hankinson <shankinson@gmail.com>
2025-06-19 14:11:01 -0300

    add aerodrom quote and swap tool

--- tutorial1/quoteAero.ts
+++ tutorial1/quoteAero.ts
@@ -7,62 +7,6 @@ const abi = [
         components: [
           { internalType: "address", name: "tokenIn", type: "address" },
           { internalType: "address", name: "tokenOut", type: "address" },
-          { internalType: "uint256", name: "amountIn", type: "uint256" },
-          { internalType: "int24", name: "tickSpacing", type: "int24" },
-          {
-            internalType: "uint160",
-            name: "sqrtPriceLimitX96",
-            type: "uint160",
-          },
-        ],
-        internalType: "struct IQuoterV2.QuoteExactInputSingleParams",
-        name: "params",
-        type: "tuple",
-      },
-    ],
-    name: "quoteExactInputSingle",
-    outputs: [
-      { internalType: "uint256", name: "amountOut", type: "uint256" },
-      { internalType: "uint160", name: "sqrtPriceX96After", type: "uint160" },
-      {
-        internalType: "uint32",
-        name: "initializedTicksCrossed",
-        type: "uint32",
-      },
-      { internalType: "uint256", name: "gasEstimate", type: "uint256" },
-    ],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [
-      { internalType: "bytes", name: "path", type: "bytes" },
-      { internalType: "uint256", name: "amountOut", type: "uint256" },
-    ],
-    name: "quoteExactOutput",
-    outputs: [
-      { internalType: "uint256", name: "amountIn", type: "uint256" },
-      {
-        internalType: "uint160[]",
-        name: "sqrtPriceX96AfterList",
-        type: "uint160[]",
-      },
-      {
-        internalType: "uint32[]",
-        name: "initializedTicksCrossedList",
-        type: "uint32[]",
-      },
-      { internalType: "uint256", name: "gasEstimate", type: "uint256" },
-    ],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        components: [
-          { internalType: "address", name: "tokenIn", type: "address" },
-          { internalType: "address", name: "tokenOut", type: "address" },
           { internalType: "uint256", name: "amount", type: "uint256" },
           { internalType: "int24", name: "tickSpacing", type: "int24" },
           {
@@ -88,17 +32,6 @@ const abi = [
       { internalType: "uint256", name: "gasEstimate", type: "uint256" },
     ],
     stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [
-      { internalType: "int256", name: "amount0Delta", type: "int256" },
-      { internalType: "int256", name: "amount1Delta", type: "int256" },
-      { internalType: "bytes", name: "path", type: "bytes" },
-    ],
-    name: "uniswapV3SwapCallback",
-    outputs: [],
-    stateMutability: "view",
     type: "function",
   },
 ];
